1f906426f4ded0c41751621081448577468c18cc
petteri.kivimaki <petteri.kivimaki@niis.org>
2026-02-28 07:52:02 +0200

    Update version number, changelog and copyright headers

--- plg_embed_google_map/src/Extension/EmbedGoogleMapParameters.php
+++ plg_embed_google_map/src/Extension/EmbedGoogleMapParameters.php
@@ -2,7 +2,7 @@
 namespace Joomla\Plugin\Content\EmbedGoogleMap\Extension;
 
 /**
- * @copyright   Copyright (C) 2014-2025 Petteri Kivimäki. All rights reserved.
+ * @copyright   Copyright (C) 2014-2026 Petteri Kivimäki. All rights reserved.
  * @license     GNU General Public License version 3; see LICENSE
  * @author      Petteri Kivimäki
  */
